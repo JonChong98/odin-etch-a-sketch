5c545f5305ecb4a699e83666788ecf38e21c94fb
Jonathan Chong <jonchong98@gmail.com>
2023-06-08 14:53:47 +0800

    Add functions to shade grid squares and change grid dimensions

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function spawnGrid(length) {
+// Spawns a length x length grid based on input
+function spawnGrid() {
+    clearGrid();
     const grid = document.querySelector(".grid");
+    let length = retrieveInput();
     const squareLength = grid.clientHeight / length;
     for (let i = 0; i < length; i++) {
         let row = document.createElement("div");
@@ -7,6 +10,7 @@ function spawnGrid(length) {
         for (let i = 0; i < length; i++) {
             let square = document.createElement("div");
             square.style = `border-bottom: 1px solid grey; border-right: 1px solid grey; flex-grow: 1;`;
+            square.addEventListener("mouseover", () => shadeSquare(square));
             row.appendChild(square);
         }
         grid.appendChild(row);
@@ -14,4 +18,22 @@ function spawnGrid(length) {
     
 }
 
-spawnGrid(16);
+function shadeSquare(square) {
+    square.classList.add("shaded");
+}
+
+function retrieveInput() {
+    let length = document.querySelector(".userInput").value;
+    length = parseInt(length, 10);
+    return length;
+}
+
+function clearGrid() {
+    const grid = document.querySelector(".grid");
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild);
+        console.log("removed child");
+    }
+}
+
+spawnGrid();
