9f3bb1106b27af9c452a5a213cef9dcacf4eb19d
Jonathan Chong <jonchong98@gmail.com>
2023-06-09 13:58:19 +0800

    Add different colouring modes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const grid = document.querySelector(".grid");
+let colourMode = "bw";
+
 // Spawns a length x length grid based on input
 function spawnGrid() {
     clearGrid();
-    const grid = document.querySelector(".grid");
     let length = retrieveInput();
     const squareLength = grid.clientHeight / length;
     for (let i = 0; i < length; i++) {
@@ -10,7 +12,7 @@ function spawnGrid() {
         for (let i = 0; i < length; i++) {
             let square = document.createElement("div");
             square.style = `border-bottom: 1px solid grey; border-right: 1px solid grey; flex-grow: 1;`;
-            square.addEventListener("mouseover", () => shadeSquare(square));
+            square.addEventListener("mouseover", (e) => shadeSquare(e.target));
             row.appendChild(square);
         }
         grid.appendChild(row);
@@ -18,22 +20,48 @@ function spawnGrid() {
     
 }
 
-function shadeSquare(square) {
-    square.classList.add("shaded");
+// Removes all divs in the grid
+function clearGrid() {
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild);
+    }
 }
 
+// Shades the square passed into the function depending on the colourMode var value
+function shadeSquare(square) {
+    square.className = "";
+    if (colourMode === "bw") {
+        square.classList.add("blackShade");
+    } else if (colourMode === "shading") {
+        if (square.style.backgroundColor === "") {
+            square.style.backgroundColor = "rgb(240, 240 ,240)";
+        } else {
+            let currRGBValue = parseInt(square.style.backgroundColor.substring(4, 7), 10);
+            if (currRGBValue > 0) {
+                let newRGBValue = currRGBValue - 30;
+                square.style.backgroundColor = `rgb(${newRGBValue}, ${newRGBValue}, ${newRGBValue})`;            
+            }
+        }
+    } else {
+        square.style.backgroundColor = `rgb(${rgbValueGen()}, ${rgbValueGen()}, ${rgbValueGen()})`;
+    }
+}
+
+// Retrieves user input from the text field
 function retrieveInput() {
     let length = document.querySelector(".userInput").value;
     length = parseInt(length, 10);
     return length;
 }
 
-function clearGrid() {
-    const grid = document.querySelector(".grid");
-    while (grid.hasChildNodes()) {
-        grid.removeChild(grid.firstChild);
-        console.log("removed child");
-    }
+// Sets the colourMode variable to the provided string
+function setColourMode(mode) {
+    colourMode = mode;
+}
+
+// Randomly generates a value betwen 0 and 255 for RGB values
+function rgbValueGen() {
+    return Math.trunc((Math.random() * 255) + 1);
 }
 
 spawnGrid();
